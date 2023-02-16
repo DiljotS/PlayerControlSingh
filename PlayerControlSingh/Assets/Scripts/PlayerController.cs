7dc506dee5e88b6ca399187a1641c11f4c144f71
Singh <730222@student.dusd.net>
2023-02-16 11:37:36 -0800

    Project finished
    Finished challenge and Bonus features

--- PlayerControlSingh/Assets/Scripts/PlayerController.cs
+++ PlayerControlSingh/Assets/Scripts/PlayerController.cs
@@ -5,10 +5,13 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
     //private variables
-    private float speed = 5.0f;
-    private float turnSpeed;
+    private float speed = 20.0f;
+    private float turnSpeed = 45.0f;
     private float horizontalInput;
     private float forwardInput;
+    public Camera mainCamera;
+    public Camera hoodCamera;
+    public KeyCode switchKey;
     // Start is called before the first frame update
     void Start()
     {
@@ -25,5 +28,12 @@ public class PlayerController : MonoBehaviour
         transform.Translate(Vector3.forward * Time.deltaTime * speed * forwardInput);
         //We turn the vehicle
         transform.Rotate(Vector3.up, turnSpeed * horizontalInput * Time.deltaTime);
+
+        if (Input.GetKeyDown(switchKey))
+        {
+            mainCamera.enabled = !mainCamera.enabled;
+            hoodCamera.enabled = !hoodCamera.enabled;
+        }
+
     }
 }
